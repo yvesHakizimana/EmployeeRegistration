3f5b899633a53ccc906fd57997aca71b9cd9c0d3
Yves-HAKIZIMANA <hkzmnyves1@gmail.com>
2024-01-27 18:18:58 +0200

    Creating employee Data Access Object and adding Setters

--- src/main/java/com/code/empregistration/employeeregistration/model/Employee.java
+++ src/main/java/com/code/empregistration/employeeregistration/model/Employee.java
@@ -10,6 +10,8 @@ public class Employee implements Serializable {
     private String lastName;
     private String username;
     private String password;
+
+
     private String contact;
     public String getFirstName() {
         return firstName;
@@ -35,5 +37,29 @@ public class Employee implements Serializable {
         return address;
     }
     private String address;
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
 }
